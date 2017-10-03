0cc0f5f71301398e8b0be8c70f994242bdb2e12b
darren <764190362@qq.com>
2017-10-03 15:19:05 +0800

    最简单的API，还没用到ApiController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreApiExamples.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreApiExamples.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository repository { get; set; }
+
+        public HomeController(IRepository repo)
+        {
+            repository = repo;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View(repository.Reservations);
+        }
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Controllers/ReservationController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using AspNetCoreApiExamples.Models;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AspNetCoreApiExamples.Controllers
+{
+    [Route("api/[controller]")]
+    public class ReservationController : Controller
+    {
+        private IRepository reposiory;
+
+        public ReservationController(IRepository repo)
+        {
+            reposiory = repo;
+        }
+
+        [HttpGet]
+        public IEnumerable<Reservation> Get()
+        {
+            return reposiory.Reservations;
+        }
+
+        [HttpGet("{id}")]
+        public Reservation Get(int id)
+        {
+            return reposiory[id];
+        }
+
+        [HttpPost]
+        public Reservation Post([FromBody]Reservation res)
+        {
+            return reposiory.AddReservation(new Reservation
+            {
+                ClientName = res.ClientName,
+                Location = res.Location
+            });
+        }
+
+        [HttpPut]
+        public Reservation Put([FromBody]Reservation res)
+        {
+           return reposiory.UpdateReservation(res);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            reposiory.DeleteReservation(id);
+        }
+    }
+}
